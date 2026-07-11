b05e18290a4083d2762214a0d9d259e6326b11a0
abdulhannan2741-sudo <abdulhannan2741@gmail.com>
2026-07-10 23:36:10 -0700

    Add Android manifest, resources, Kotlin MainActivity, and assets

---
+++ android/app/src/main/kotlin/com/hanan/scanner/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hanan.scanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ assets/images/README.md
@@ -0,0 +1,2 @@
+# This directory is reserved for app image assets.
+# Add any PNG/SVG assets here and reference them in pubspec.yaml.
